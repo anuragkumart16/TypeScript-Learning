885ec32410a6b342a61cdb58072613f4fbc3e375
anuragkumart16 <114322758+anuragkumart16@users.noreply.github.com>
2025-12-07 09:11:14 +0530

    removed dist folder

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function greet(name) {
-    return `Hello, ${name}!`;
-}
-console.log(greet("Anurag"));
-//# sourceMappingURL=index.js.map
